8d68756375e0483c7ac2b4d6bbbece420dbbb495
Peter Shen <xianpeng.shen@gmail.com>
2022-06-19 06:15:18 -0600

    Squashed Updates (#10)
    - prep repo as CMake project
    - workflows to use as a python pkg or GH action
    - rename workflows
    
    Co-authored-by: Brendan <2bndy5@gmail.com>

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,15 +1,16 @@
 /** This is a very ugly test code (doomed to fail linting) */
 #include "demo.hpp"
 #include <cstdio>
+#include <cstddef>
+
+size_t dummyFunc(size_t i) { return i; }
 
 int main()
 {
     for (;;)
         break;
 
-
     printf("Hello world!\n");
 
     return 0;
 }
-
